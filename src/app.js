3a3c4652147ba691c0835702c01bbaf3bcd524dd
William Jeuniaux <william.jeuniaux@genesys.com>
2018-03-23 15:10:22 +0100

    setup production build and server

--- src/app.js
+++ src/app.js
@@ -12,10 +12,10 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-const test = (
+const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
 
-ReactDOM.render(test, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
